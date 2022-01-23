10f90fb9aed6e452f21fcc7c8f415859de55e259
JhonNarvaezT <90275255+JhonNarvaezT@users.noreply.github.com>
2022-01-23 16:54:27 -0600

    Add files via upload

---
+++ Cap4/Cap4/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Cap4
+{
+    partial class FrmMultas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.txtVelocidad = new System.Windows.Forms.TextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.lvMultas = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Control De Registro De Multas De Transito";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(115, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero De Placa :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " Ingrese Velocidad (km/h) :";
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(139, 83);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(100, 20);
+            this.txtPlaca.TabIndex = 3;
+            // 
+            // txtVelocidad
+            // 
+            this.txtVelocidad.Location = new System.Drawing.Point(139, 156);
+            this.txtVelocidad.Name = "txtVelocidad";
+            this.txtVelocidad.Size = new System.Drawing.Size(100, 20);
+            this.txtVelocidad.TabIndex = 4;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(449, 89);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(116, 21);
+            this.lblFecha.TabIndex = 5;
+            this.lblFecha.Text = "Fecha Actual :";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(452, 125);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(108, 21);
+            this.lblHora.TabIndex = 6;
+            this.lblHora.Text = "Hora Actual :";
+            // 
+            // lvMultas
+            // 
+            this.lvMultas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lvMultas.GridLines = true;
+            this.lvMultas.HideSelection = false;
+            this.lvMultas.Location = new System.Drawing.Point(58, 230);
+            this.lvMultas.Name = "lvMultas";
+            this.lvMultas.Size = new System.Drawing.Size(698, 154);
+            this.lvMultas.TabIndex = 7;
+            this.lvMultas.UseCompatibleStateImageBehavior = false;
+            this.lvMultas.View = System.Windows.Forms.View.Details;
+            this.lvMultas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lvMultas_MouseClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Numero De Placa";
+            this.columnHeader1.Width = 160;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Velocidad (km/h)";
+            this.columnHeader2.Width = 160;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Fecha";
+            this.columnHeader3.Width = 160;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Hora";
+            this.columnHeader4.Width = 160;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Multa";
+            this.columnHeader5.Width = 160;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(612, 175);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(92, 33);
+            this.btnRegistrar.TabIndex = 8;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(119, 390);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(86, 33);
+            this.btnEliminar.TabIndex = 9;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(288, 390);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(97, 33);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // FrmMultas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.lvMultas);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.txtVelocidad);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMultas";
+            this.Text = "FrmMultas";
+            this.Load += new System.EventHandler(this.FrmMultas_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.TextBox txtVelocidad;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.ListView lvMultas;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Cap4/Cap4/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cap4
+{
+    public partial class FrmMultas : Form
+    {
+        ListViewItem item;
+        public FrmMultas()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMultas_Load(object sender, EventArgs e)
+        {
+            lblFecha.Text=DateTime.Today.ToShortDateString();
+            lblHora.Text=DateTime.Now.ToShortTimeString();
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            String Placa = txtPlaca.Text;
+            double Velocidad = double.Parse(txtVelocidad.Text);
+            DateTime Fecha = DateTime.Parse(lblFecha.Text);
+            DateTime Hora = DateTime.Parse(lblHora.Text);
+
+            double Multa = 0;
+            if (Velocidad<=70)
+                Multa=0;
+            else if (Velocidad >70 && Velocidad <=90)
+                Multa=120;
+            else if (Velocidad>90 && Velocidad <=100)
+                Multa=320;
+            else if (Velocidad>100)
+                Multa=500;
+
+            ListViewItem Fila = new ListViewItem(Placa);
+            Fila.SubItems.Add(lblHora.Text);
+            Fila.SubItems.Add(lblFecha.Text);
+            Fila.SubItems.Add(Velocidad.ToString("0.00"));
+            Fila.SubItems.Add(Multa.ToString(".00"));
+            lvMultas.Items.Add(Fila);
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (item != null)
+            {
+                lvMultas.Items.Remove(item);
+                MessageBox.Show("Su multa ha sido eliminada exitosamente....!!!");
+            }else
+            {
+                MessageBox.Show("Debe seleccionar una multa de la lista");
+            }
+        }
+
+        private void lvMultas_MouseClick(object sender, MouseEventArgs e)
+        {
+            item= lvMultas.GetItemAt(e.X, e.Y);
+        }
+    }
+}
